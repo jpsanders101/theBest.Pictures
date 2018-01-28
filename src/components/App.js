5241d7bc8e8aba838730ae7dcc402be762c728d7
Jon Sanders <jonsanders101@aol.com>
2018-01-28 20:08:28 +0000

    Add high-level app component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>A place to tick off Oscar best picture winners as I watch them.</h1>
+        This is where App goes
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill';
+import {Router, browserHistory} from 'react-router';
+import React from 'react';
+import { render } from  'react-dom';
+import App from './components/App';
+import './styles/style.css';
+
+render (
+  <App />,
+  document.getElementById('app')
+);
