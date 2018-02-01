2f2eaeb73da2ba8ada3fd3f98bacd2fb730439db
Jon Sanders <jonsanders101@aol.com>
2018-02-01 22:54:03 +0000

    Add more films

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Homepage from './Homepage';
 
-const bestPictureWinners = [{name: 'Wings', releaseYear: '1927'}, {name: 'The Broadway Melody', releaseYear: 1928},  {name: 'All Quiet On the Western Front', releaseYear: 1930} ]
-
+const bestPictureWinners = [{name: 'Wings', releaseYear: '1927'},
+                            {name: 'The Broadway Melody', releaseYear: 1928},
+                            {name: 'All Quiet On the Western Front', releaseYear: 1930},
+                            {name: 'Cimarron', releaseYear: 1931},
+                            {name: 'Grand Hotel', releaseYear: 1932},
+                            {name: 'Cavalcade', releaseYear: 1933}]
+;
 class App extends React.Component {
   render() {
     return (
