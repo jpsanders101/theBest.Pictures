d079819e354e23711405111e259bdb3d0fe03002
Jon Sanders <jonsanders101@aol.com>
2018-02-04 23:37:46 +0000

    Create SeenSelect functional component

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -1,13 +1,31 @@
 import React, {PropTypes} from 'react';
 
 const MovieItem = (props) => {
-  return <li key={props.key}>{props.name} ({props.releaseYear})</li>;
+  return (
+    <li key={props.key}>{props.name} ({props.releaseYear})
+    {props.seen ? "" : <SeenSelect name={props.name} onClick={props.onClick}/>}
+    </li>
+  );
+};
+
+const SeenSelect = (props) => {
+
+  const onClick = e => {
+    props.onClick(props.name);
+  };
+
+  return (
+    <span onClick={onClick}>- Seen?</span>
+  );
 }
+
+
 
 MovieItem.propTypes = {
   key: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
-  releaseYear: PropTypes.number.isRequired
+  releaseYear: PropTypes.number.isRequired,
+  seen: PropTypes.bool.isRequired
 };
 
 export default MovieItem;
--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -5,7 +5,7 @@ class MovieList extends React.Component {
   constructor(props) {
     super(props);
     this.state = props;
-    this.onClick = this.onClick.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
   }
 
   render() {
@@ -14,23 +14,22 @@ class MovieList extends React.Component {
       <h1>Seen:</h1>
     <ul>
     {this.state.bestPictureWinners.filter(movie => movie.seen).map((movie, index) => {
-    return <MovieItem key={index} name={movie.name} releaseYear={movie.releaseYear}/>;
+    return <MovieItem key={index} name={movie.name} releaseYear={movie.releaseYear} onClick={this.onClickHandler}/>;
     })}
     </ul>
     <h1>Yet to See:</h1>
     <ul>
       {this.state.bestPictureWinners.filter(movie => !movie.seen).map((movie, index) => {
-        return <MovieItem key={index} name={movie.name} releaseYear={movie.releaseYear}/>;
+        return <MovieItem key={index} name={movie.name} releaseYear={movie.releaseYear} onClick={this.onClickHandler}/>;
       })}
     </ul>
     </div>);
   }
 
-  onClick(e) {
-    console.log(e.target.p);
+  onClickHandler(movieName) {
     const state = this.state;
     let index = state.bestPictureWinners.findIndex(movie => {
-      return movie.name === "Wings";
+      return movie.name === movieName;
     });
     state.bestPictureWinners[index].seen = true;
     this.setState(state);
