89ab79b0ba8d1842db561ed69e10ecbd5ae42c29
Jon Sanders <jonsanders101@aol.com>
2018-03-30 21:46:40 +0100

    Store user's picture review in component state

--- src/components/MoviePage/ReviewForm.js
+++ src/components/MoviePage/ReviewForm.js
@@ -1,20 +1,38 @@
 import React, {PropTypes} from 'react';
 import RatingButtonContainer from './RatingButtonContainer';
 
-export default function ReviewForm({ movie }) {
-  return (
-    <form name="review-form">
-      <label>
-        Rating
-        <RatingButtonContainer />
-      </label>
-      <label>
-        Thoughts about the picture...
-        <textarea></textarea>
-      </label>
-        <input type="submit" value="Save" />
-    </form>
-  );
+export default class ReviewForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      review: ""
+    };
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+  onChangeHandler(e) {
+    let state = Object.assign({}, this.state, {review: e.target.value} );
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <form name="review-form">
+        <label>
+          Rating
+          <RatingButtonContainer />
+        </label>
+        <label>
+          Thoughts about the picture...
+          <textarea
+            value={this.state.review}
+            onChange={this.onChangeHandler}
+          ></textarea>
+        </label>
+          <input type="submit" value="Save" />
+      </form>
+    );
+  }
 }
 
 ReviewForm.propTypes = {
