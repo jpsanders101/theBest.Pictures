37a77913a7517d33c9e6d7055cf4ebd82b042bb9
Jon Sanders <jonsanders101@aol.com>
2018-03-30 20:48:31 +0100

    Refactor MoviePage into functional component

--- src/components/MoviePage/index.js
+++ src/components/MoviePage/index.js
@@ -2,24 +2,18 @@ import React, {PropTypes} from 'react';
 import MovieItem from '../MovieItem';
 import ReviewSection from './ReviewSection';
 
-export default class MoviePage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(
-      <div>
-        <h1>Movie Title - Date</h1>
-        <div className="movie-page-container">
-          <div className="movie-details-section">
-            <p>Synopsis of the movie goes here.</p>
-          </div>
-          <ReviewSection movie={this.props.movie}/>
+export default function MoviePage(props) {
+  return(
+    <div>
+      <h1>Movie Title - Date</h1>
+      <div className="movie-page-container">
+        <div className="movie-details-section">
+          <p>Synopsis of the movie goes here.</p>
         </div>
+        <ReviewSection movie={props.movie}/>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 MoviePage.propTypes = {
