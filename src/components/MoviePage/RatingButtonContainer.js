55192b88bae7660a63eb82f34d5e3fbc2a927987
Jon Sanders <jonsanders101@aol.com>
2018-03-30 20:12:01 +0100

    Refactor RatingButtonContainer into new component

---
+++ src/components/MoviePage/RatingButtonContainer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import RatingButton from './RatingButton';
+
+
+export default class RatingButtonContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      highlightButtonsUpto: 0
+    }
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+  }
+
+  handleMouseEnter(e) {
+    this.setState({ highlightButtonsUpto: e.target.value });
+  }
+
+  handleMouseLeave(e) {
+    this.setState({ highlightButtonsUpto: 0 });
+  }
+
+  renderButtons (buttonQuantity = 5) {
+    const buttons = [];
+    for (let buttonCount = 1; buttonCount <= buttonQuantity; buttonCount++) {
+      const highlighted = buttonCount <= this.state.highlightButtonsUpto;
+      buttons.push(
+        <RatingButton
+          key={buttonCount}
+          value={`${buttonCount}`}
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+          highlighted={highlighted}
+        />
+      );
+    }
+    return buttons;
+  }
+
+  render() {
+    return (
+      <div className="rating-buttons">
+        {this.renderButtons()}
+      </div>
+    );
+  }
+
+}
--- src/components/MoviePage/ReviewForm.js
+++ src/components/MoviePage/ReviewForm.js
@@ -1,59 +1,21 @@
 import React from 'react';
-import RatingButton from './RatingButton';
+import RatingButtonContainer from './RatingButtonContainer';
 
-export default class ReviewForm extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      highlightButtonsUpto: 0
-    }
-    this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    this.handleMouseLeave = this.handleMouseLeave.bind(this);
-  }
-
-  handleMouseEnter(e) {
-    this.setState({ highlightButtonsUpto: e.target.value });
-  }
-
-  handleMouseLeave(e) {
-    this.setState({ highlightButtonsUpto: 0 });
-  }
-
-  renderButtons (buttonQuantity = 5) {
-    const buttons = [];
-    for (let buttonCount = 1; buttonCount <= buttonQuantity; buttonCount++) {
-      const highlighted = buttonCount <= this.state.highlightButtonsUpto;
-      buttons.push(
-        <RatingButton
-          key={buttonCount}
-          value={`${buttonCount}`}
-          onMouseEnter={this.handleMouseEnter}
-          onMouseLeave={this.handleMouseLeave}
-          highlighted={highlighted}
-        />
-      );
-    }
-    return buttons;
-  }
-
-  render () {
-    return (
-      <form name="review-form">
-        <label>
-          Rating
-          <div className="rating-buttons">
-            {this.renderButtons()}
-          </div>
-        </label>
-        <label>
-          Thoughts about the picture...
-          <textarea></textarea>
-        </label>
+export default function ReviewForm() {
+  return (
+    <form name="review-form">
+      <label>
+        Rating
+        <RatingButtonContainer />
+      </label>
+      <label>
+        Thoughts about the picture...
+        <textarea></textarea>
+      </label>
         <input type="submit" value="Save" />
-      </form>
-    );
-  }
+    </form>
+  );
 }
+
 
 
