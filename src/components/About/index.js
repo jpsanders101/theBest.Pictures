13061a51ee0d7f432a96177086a938e7a4527dd5
Jon Sanders <jonsanders101@aol.com>
2018-03-31 19:05:47 +0100

    Refactor About page: remove redundant home link, change from class to functional componen

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-class About extends React.Component {
-  render () {
-    return (
-      <div>
-        <h1>ABOUT</h1>
-        A little app used for ticking-off Oscar best-picture winners as I watch them.
-        <Link to="/">HOME</Link>
-      </div>
-    );
-  }
+export default function About(props) {
+  return (
+    <div>
+      <h1>About</h1>
+      A little app used for ticking-off Oscar best-picture winners as I watch them.
+    </div>
+  );
 }
-
-export default About;
