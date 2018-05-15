cd52202fbfc654eb78607184b2ea2291abd9ff25
Jon Sanders <jonsanders101@aol.com>
2018-05-15 22:59:56 +0100

    Add RatingButton tests

--- spec/components/RatingButton/RatingButtonSpec.js
+++ spec/components/RatingButton/RatingButtonSpec.js
@@ -3,8 +3,35 @@ import { shallow } from 'enzyme';
 import RatingButton from '../../../src/components/RatingButton';
 
 describe('RatingButton', () => {
+  let wrapper;
   const onClickSpy = jasmine.createSpy();
-  const onClick = onClickSpy;
-  const props = {};
-  const wrapper = shallow(<RatingButton />);
+  const onEnterSpy = jasmine.createSpy();
+  const onLeaveSpy = jasmine.createSpy();
+
+  beforeEach(() => {
+    const props = {
+      highlighted: false,
+      onClick: onClickSpy,
+      onMouseEnter: onEnterSpy,
+      onMouseLeave: onLeaveSpy,
+      value: 1
+    };
+    wrapper = shallow(<RatingButton {...props} />);
+  });
+
+  it('SHOULD call click handler', () => {
+    wrapper.simulate('click');
+    expect(onClickSpy).toHaveBeenCalled();
+  });
+  it('SHOULD call onEnter handler', () => {
+    wrapper.simulate('mouseEnter');
+    expect(onEnterSpy).toHaveBeenCalled();
+  });
+  it('SHOULD call onLeave handler', () => {
+    wrapper.simulate('mouseLeave');
+    expect(onLeaveSpy).toHaveBeenCalled();
+  });
+  it('SHOULD display its value', () => {
+    expect(wrapper.text()).toEqual('1');
+  });
 });
