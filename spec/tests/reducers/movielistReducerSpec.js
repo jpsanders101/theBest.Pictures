2b4ee7c9ccb105c9d033bdbcda8f1db6f1bb1ac7
Jon Sanders <jonsanders101@aol.com>
2018-05-25 21:24:45 +0100

    Add tests for movielistReducer

---
+++ spec/tests/reducers/movielistReducerSpec.js
@@ -0,0 +1,55 @@
+import { moviesData } from '../../data';
+import proxyquire from 'proxyquire';
+
+proxyquire.noCallThru();
+
+const saveReviewSpy = jasmine.createSpy('saveReviewSpy');
+const helpersMock = { saveReview: saveReviewSpy };
+
+const movielistReducer = proxyquire('../../../src/reducers/movielistReducer', {
+  './helpers': helpersMock
+}).default;
+
+const actionMovies = [...moviesData];
+
+describe('movielistReducer', () => {
+  describe('GIVEN a MARK_AS_SEEN action was dispatched', () => {
+    it('SHOULD return correctly updated state', () => {
+      expect(
+        movielistReducer(moviesData, {
+          type: 'MARK_AS_SEEN',
+          movies: actionMovies
+        })
+      ).toEqual(actionMovies);
+    });
+  });
+  describe('GIVEN a LOAD_MOVIES action was dispatched', () => {
+    it('SHOULD return correctly updated state', () => {
+      expect(
+        movielistReducer(moviesData, {
+          type: 'LOAD_MOVIES',
+          movies: actionMovies
+        })
+      ).toBe(actionMovies);
+    });
+  });
+  describe('GIVEN a SAVE_REVIEW action was dispatched', () => {
+    it('SHOULD return correctly updated state', () => {
+      movielistReducer(moviesData, {
+        type: 'SAVE_REVIEW',
+        review: 'Review'
+      });
+      expect(saveReviewSpy).toHaveBeenCalledWith(moviesData, 'Review');
+    });
+  });
+  describe('GIVEN an action of some other type has been dispatched', () => {
+    it('SHOULD return state without update', () => {
+      expect(
+        movielistReducer(moviesData, {
+          type: 'SOMETHING_ELSE',
+          data: 'something else'
+        })
+      ).toBe(moviesData);
+    });
+  });
+});
