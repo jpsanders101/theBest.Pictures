a1f174aebeb547a5efd7e07dcc2b3b6e23c7f0d4
Jon Sanders <jonsanders101@aol.com>
2018-06-10 14:13:17 +0100

    Display percentage of films seen in centre of progress bar

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -3,14 +3,17 @@ import PropTypes from 'prop-types';
 
 export default function ProgressBar(props) {
   return (
-    <div className="progress-bar">
-      <div
-        className="progress-bar_filler--completed"
-        style={{
-          flex: `0 1 ${props.progress}%`
-        }}
-      />
-      <div className="progress-bar_filler--remaining" />
+    <div className="progress-bar_container">
+      <div className="progress-bar">
+        <div className="progress-bar_percent">{props.progress}%</div>
+        <div
+          className="progress-bar_filler--completed"
+          style={{
+            flex: `0 1 ${props.progress}%`
+          }}
+        />
+        <div className="progress-bar_filler--remaining" />
+      </div>
     </div>
   );
 }
