648b132912bb107132182580b7de15bfa32439a2
jonsanders101 <jonsanders101@aol.com>
2018-11-18 11:37:08 +0000

    Jazz-up the start up message

--- tools/startMessage.js
+++ tools/startMessage.js
@@ -2,6 +2,21 @@ import colors from 'colors';
 
 /* eslint-disable no-console */
 
+const nodeEnv = process.env.NODE_ENV;
+
+const modeConsoleOutput = () => {
+  if (nodeEnv === 'production') {
+    return 'PRODUCTION'.magenta;
+  }
+  if (nodeEnv === 'development') {
+    return 'DEV'.cyan;
+  }
+};
+
 console.log(
-  `Starting TheBest.Pictures in ${process.env.NODE_ENV} mode...`.green
+  'Starting'.green,
+  'TheBest.Pictures'.green.bold,
+  'in'.green,
+  modeConsoleOutput(),
+  'mode...'.green
 );
