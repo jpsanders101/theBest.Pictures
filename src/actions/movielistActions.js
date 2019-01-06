2e9a339e6d25f259aad1bd746b58f8e6f7339d6f
jonsanders101 <jonsanders101@aol.com>
2019-01-06 14:54:49 +0000

    Create getMovies request and use it instead of mock API

--- src/actions/movielistActions.js
+++ src/actions/movielistActions.js
@@ -2,6 +2,7 @@ import * as types from './constants.js';
 import bestPictureWinnersApi from '../api/bestPictureWinnersApi';
 import { beginAjaxCall, endAjaxCall } from './ajaxCallActions';
 import { setErrorState, removeErrorState } from './appActions';
+import { getMovies } from '../requests/get-movies';
 
 export const markAsSeen = movies => {
   return { type: types.MARK_AS_SEEN, movies };
@@ -14,8 +15,7 @@ export const loadMoviesSuccess = movies => {
 export const loadMovies = () => {
   return dispatch => {
     dispatch(beginAjaxCall(1));
-    return bestPictureWinnersApi
-      .getAllMovies()
+    return getMovies()
       .then(movies => {
         dispatch(loadMoviesSuccess(movies));
       })
---
+++ src/requests/get-movies.js
@@ -0,0 +1,16 @@
+import http from 'http';
+
+export const getMovies = () => {
+
+    return new Promise((resolve, reject) => {
+        http.get('http://localhost:8080/get-movies', res => {
+            let moviesData = '';
+            res.on('data', chunk => {
+                moviesData += chunk;
+            }).on('end', () => {
+                resolve(JSON.parse(moviesData));
+            });
+        });
+    });
+
+};
