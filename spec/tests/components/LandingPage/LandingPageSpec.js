082ab6628107503bbc5521c414665ee3e39c267a
jonsanders101 <jonsanders101@aol.com>
2019-06-06 13:06:32 +0100

    Fix LandingPage unit test

--- spec/tests/components/LandingPage/LandingPageSpec.js
+++ spec/tests/components/LandingPage/LandingPageSpec.js
@@ -1,15 +1,9 @@
-// import ConnectedLandingPage from '../../../../src/components/LandingPage';
+import ConnectedLandingPage from '../../../../src/components/LandingPage';
 import { shallow } from 'enzyme';
 import React from 'react';
 
 Proxyquire.noCallThru();
 
-const document = {};
-const ConnectedLandingPage = Proxyquire(
-  '../../../../src/components/LandingPage',
-  {},
-  { document, console: 'hi' }
-).default;
 const dismissLandingPageSpy = jasmine.createSpy('dismissLandingPageSpy');
 const LandingPage = ConnectedLandingPage.WrappedComponent;
 const wrapper = shallow(
@@ -25,13 +19,13 @@ fdescribe('<LandingPage />', () => {
     beforeEach(() => {
       wrapper
         .find('.landing-page__button')
-        .simulate('click', { preventDefault: () => {} });
+        .simulate('click', { preventDefault: () => { } });
     });
     it('SHOULD call the dismissLandingPage prop', () => {
       expect(dismissLandingPageSpy).toHaveBeenCalled();
     });
-    it('SHOULD set the seen_landing cookie', () => {
-      expect(document.cookie).toEqual('seen_landing=true');
+    fit('SHOULD set the seen_landing cookie', () => {
+      expect(global.document.cookie).toEqual('seen_landing=true');
     });
   });
 });
