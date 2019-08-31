0ddd3ad9a44131c9a979713782d245a63bca5b01
Jon Sanders <jonsanders101@aol.com>
2019-08-31 13:15:18 +0100

    Change MovieList to functional component

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -4,31 +4,24 @@ import MovieItem from '../MovieItem';
 import { connect } from 'react-redux';
 import { markAsSeen } from '../../actions/movielistActions';
 
-class MovieList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleMovieClick = this.handleMovieClick.bind(this);
-    this.handleFilterClick = this.handleFilterClick.bind(this);
-  }
-
-  handleMovieClick(movieName) {
-    const movieList = [...this.props.movies];
+function MovieList(props) {
+  const handleMovieClick = movieName => {
+    const movieList = [...props.movies];
     let index = movieList.findIndex(movie => {
       return movie.name === movieName;
     });
     movieList[index].seen = true;
-    this.props.markAsSeen(movieList);
-  }
+    props.markAsSeen(movieList);
+  };
 
-  renderMovieList() {
-    return this.props.movies
+  const renderMovieList = () => {
+    return props.movies
       .filter(movie => {
-        if (this.props.filters.seen === 'none') {
+        if (props.filters.seen === 'none') {
           return true;
-        } else if (this.props.filters.seen === 'seen') {
+        } else if (props.filters.seen === 'seen') {
           return movie.seen;
-        } else if (this.props.filters.seen === 'unseen') {
+        } else if (props.filters.seen === 'unseen') {
           return !movie.seen;
         }
       })
@@ -36,28 +29,18 @@ class MovieList extends React.Component {
         <MovieItem
           key={movie.awardNumber}
           name={movie.name}
-          onClick={this.handleMovieClick}
+          onClick={handleMovieClick}
           releaseYear={movie.releaseYear}
           seen={movie.seen}
         />
       ));
-  }
+  };
 
-  handleFilterClick(e) {
-    if (this.props.filters.seen === e.target.dataset.filter) {
-      this.setState({ filter: 'none' });
-    } else {
-      this.setState({ filter: e.target.dataset.filter });
-    }
-  }
-
-  render() {
-    return (
-      <div className="movie-list__container">
-        <ul className="movie-list">{this.renderMovieList()}</ul>
-      </div>
-    );
-  }
+  return (
+    <div className="movie-list__container">
+      <ul className="movie-list">{renderMovieList()}</ul>
+    </div>
+  );
 }
 
 MovieList.propTypes = {
