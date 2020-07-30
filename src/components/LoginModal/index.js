4cc024e8df696e225cfaec0f17eeb6bd47eca71f
Jon Sanders <jpsanders101@gmail.com>
2020-07-30 11:02:30 +0100

    Add basic login form.

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Formik } from 'formik';
 
 export default function LoginModal() {
   return (
     <section className="login-modal-container">
-      <form className="login-modal">
-        <input type="text" />
-        <input type="text" />
-        <button type="submit">Submit</button>
-      </form>
+      <Formik initialValues={{ email: '', password: '' }}>
+        {props => (
+          <form className="login-modal">
+            <input
+              onChange={props.handleChange}
+              value={props.values.email}
+              name="email"
+              type="text"
+            />
+            <input
+              onChange={props.handleChange}
+              value={props.values.password}
+              name="password"
+              type="text"
+            />
+            <button type="submit">Login</button>
+          </form>
+        )}
+      </Formik>
     </section>
   );
 }
